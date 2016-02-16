1365cf64fa19d2047f09cc23a78b9679a7c586aa
ziphrax <daverival@hotmail.com>
2016-02-16 02:02:18 +0000

    Added filter to terraced api and some error handlng changes

--- webservice/routes/terraced.js
+++ webservice/routes/terraced.js
@@ -5,11 +5,21 @@ var router = express.Router();
 router.get('/',function(req,res){
   Terraced.find({}).exec(function(err,docs){
     if(err){
-      res.status(500).send(err.message);
+      res.status(500).json({status:500,message: err.message, url: '/terraced'});
     } else {
-      res.json(docs);
+      res.json({status:200,message: 'ok',data: docs, url: '/terraced'});
     }
-  });  
+  });
+})
+  .get('/:region',function(req,res){
+    console.log(req.params.region);
+  Terraced.find({}).select('_id Year Quater Period ' + req.params.region.toUpperCase()).exec(function(err,docs){
+    if(err){
+      res.status(500).json({status:500,message: err.message, url: '/terraced/' + req.params.region.toUpperCase()});
+    } else {
+      res.json({status:200,message: 'ok', data: docs, url: '/terraced/' + req.params.region.toUpperCase()});
+    }
+  });
 });
 
 module.exports = router;
